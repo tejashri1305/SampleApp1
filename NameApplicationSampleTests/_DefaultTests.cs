18dd626ea09ad1010f9947843bcbc9aca514f709
Tejashri Dilip Kolapkar <tejashri_kolapkar@infosys.com>
2019-01-30 17:16:06 +0530

    chk in unit tests project

---
+++ NameApplicationSampleTests/_DefaultTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NameApplicationSample;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NameApplicationSample.Tests
+{
+    [TestClass()]
+    public class _DefaultTests
+    {
+        [TestMethod()]
+        public void btnName_ClickTestMethod()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
